c69b3612dfa5f998bf304dae471ebc11a071dfe4
Arm-21 <=>
2024-08-27 18:33:30 +0545

    reading content and writing content using fs module of node

---
+++ Section_1/reading_content_from_file/index.js
@@ -0,0 +1,12 @@
+import fs from "node:fs";
+import {exec} from "node:child_process";
+// fs.writeFileSync("./content.txt")
+// //for path in windows we use \\ is considered-> \ ,(only \ is considered as escape sequence character )
+// fs.appendFileSync("./content.txt","\nhello this is a me2") 
+
+
+// const data = fs.readFileSync('./content.txt');
+// console.log(data.toString())
+// fs.renameSync("./content.txt" , "./uncontent.txt")
+// fs.unlinkSync('./uncontent.txt');
+// exec("code")
