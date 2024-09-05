ba56c301e43e73f0611a0952dea5666c0a1f78a5
Arm-21 <=>
2024-09-05 09:11:08 +0545

    reading content from files and setting environment variables

--- Section_2/OS_Info_process/what_are_environments_variable/env.js
+++ Section_2/OS_Info_process/what_are_environments_variable/env.js
@@ -1,2 +1,21 @@
-const env = process.env;
-console.log(env)
+// import {exec} from "child_process";
+import fs from "node:fs"
+// const env = process.env;
+// console.log(env)
+
+
+//We use exec from our child_process to execute a command in terminal from node
+// exec('ls',(err)=>{
+//     if(err){
+//         console.log(err)
+//     }
+// })
+// console.log(h)
+
+const content = fs.readFileSync('./abc').toString();
+content.split('\n').forEach((val)=>{
+const [key,value]=val.split('=');
+process.env[key] = value;
+});
+
+console.log(process.env)
