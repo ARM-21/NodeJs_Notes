f4620cff45b8c5a6a3b8d87da8f2e2d2d2e5e5fe
arm-21 <manojneupane787@gmail.com>
2024-10-23 10:29:58 +0545

    reading image in base64

--- section_6/base64_encoding/app.js
+++ section_6/base64_encoding/app.js
@@ -1,8 +1,18 @@
 import fs from 'node:fs/promises';
 
 //it is equivaltent to btoa in browser
-const a = await fs.readFile('file.txt', 'base64')
-console.log(a)
-
+// const a = await fs.readFile('pic.jpg', 'base64')
+// // console.log(a)
+// fs.writeFile('new-pic.txt', a)
 //it is equivaltent to atob in browser
-fs.writeFile('new-file.txt', a, 'base64')
+// fs.writeFile('new-file.txt', a, 'base64')
+
+
+
+/**recovering image from base64 string */
+
+const str = await fs.readFile('new-pic.txt', 'utf-8')
+console.log(str)
+
+// const img = str.toString('utf-8')
+fs.writeFile('new-pic.jpg', str, 'base64')
