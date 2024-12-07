9ac18d735fd590cac7e98d64851e2aca7dba6e9b
arm-21 <manojneupane787@gmail.com>
2024-12-07 22:48:53 +0545

    streams on javas and networking started

--- section_8/27_streams-in-javascript/backend/app.js
+++ section_8/27_streams-in-javascript/backend/app.js
@@ -3,16 +3,18 @@ import fs from "fs/promises";
 
 const server = http.createServer(async (req, res) => {
   res.setHeader("access-control-allow-origin", "*");
-  res.setHeader("Content-Type", "video/mp4");
-  res.setHeader("Content-Disposition", "attachment; filename=streams.mp4");
+  // res.setHeader("Content-Type", "plain/tx");
+  res.setHeader("content-Type","image/webp");
+  
+  // res.setHeader("Content-Disposition", "attachment; filename=streams.mp4");
 
   const fileHandle = await fs.open(
-    "C:\\Users\\anura\\OneDrive\\Desktop\\streams.mp4"
+    "./river.webp"
   );
-  const { size } = await fileHandle.stat();
-  res.setHeader("Content-Length", size);
+  // const { size } = await fileHandle.stat();
+  // res.setHeader("Content-Length", size);
   const readStream = fileHandle.createReadStream({
-    highWaterMark: 10 * 1024 * 1024,
+    highWaterMark: 10*1024*1024,
   });
 
   readStream.on("data", (chunk) => {
@@ -22,7 +24,7 @@ const server = http.createServer(async (req, res) => {
 
     setTimeout(() => {
       readStream.resume();
-    }, 50);
+    }, 100);
   });
 
   readStream.on("end", () => {
@@ -31,5 +33,5 @@ const server = http.createServer(async (req, res) => {
 });
 
 server.listen(4000, "localhost", () => {
-  console.log("Server Started");
+  console.log("Server Started"+ server.address().port);
 });
