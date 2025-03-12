05d91ce59264ab537ca4376c0b8c2fa7ad854075
arm-21 <manojneupane787@gmail.com>
2025-03-12 21:39:33 +0545

    path module

--- section_11/Drive_using_express/path.mjs
+++ section_11/Drive_using_express/path.mjs
@@ -1,10 +1,10 @@
 import path from "node:path";
 
-console.log(path.join("/../name","/tax"))
+console.log(path.join("/","./tax/../../"))
 
 
 //into normal form removes unecessary part
-console.log(path.normalize("../../name"))
+console.log(path.normalize("./name/../.."))
 
 
 console.log(path.resolve("form","to"))
@@ -12,6 +12,6 @@ console.log(path.resolve("./to"))
 
 //converts the to to an absolute path by joining form
 
-
+//from whereever we are executing
 console.log(path.basename('name/images/hello.png')) //hello.png
 console.log(path.dirname('name/images/hello.png')) //name/images
